e1d11e91b55bb997b83fd52f279b81ef1a2390c0
Johan Brattemark <johan.brattemark@gmail.com>
2017-07-13 16:45:50 -0700

    Make 'select all' button change depending on amount of messages selected

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -3,7 +3,25 @@ import './index.css';
 
 class Toolbar extends Component {
   render() {
-    let messageCount = this.props.messages.filter((msg) => !msg.read).length;
+    // let unreadMessagesCount = this.props.messages.filter((msg) => !msg.read).length;
+    // let selectedMessagesCount = this.props.messages.filter((msg) => msg.selected).length;
+    let totalMessagesCount = this.props.messages.length;
+    let unreadMessagesCount = 0;
+    let selectedMessagesCount = 0;
+    for (let msg of this.props.messages) {
+      if (!msg.read) {unreadMessagesCount += 1};
+      if (msg.selected) {selectedMessagesCount += 1};
+    }
+
+    // Set 'select all' button appearance depending on selected messages count
+    let selectAllButtonClass = 'fa fa-square-o';
+    if (selectedMessagesCount === 0) {
+      selectAllButtonClass = 'fa fa-square-o';
+    } else if (selectedMessagesCount === totalMessagesCount) {
+      selectAllButtonClass = 'fa fa-check-square-o'; // all messages selected
+    } else if (selectedMessagesCount > 0) {
+      selectAllButtonClass = 'fa fa-minus-square-o'; // some messages selected
+    }
 
     return (
       <div className="Toolbar">
@@ -11,12 +29,12 @@ class Toolbar extends Component {
         <div className="row toolbar">
         <div className="col-md-12 text-left">
         <p className="pull-right">
-        <span className="badge badge">{messageCount}</span>
+        <span className="badge badge">{unreadMessagesCount}</span>
         unread messages
         </p>
 
         <button className="btn btn-default">
-        <i className="fa fa-square-o" onClick={() => this.props.selectAllMessages()}></i>
+        <i className={selectAllButtonClass} onClick={this.props.selectAllMessages}></i>
         </button>
 
         <button className="btn btn-default" disabled="disabled">
