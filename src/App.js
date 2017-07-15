c2a8ff2f5c277a15f6474d556b4d6b746946c7f6
Johan Brattemark <johan.brattemark@gmail.com>
2017-07-14 20:51:34 -0700

    Change from promise-based fetch to async-await fetch, just for fun

--- src/App.js
+++ src/App.js
@@ -3,67 +3,6 @@ import './App.css';
 import Toolbar from './Toolbar.js';
 import MessageTable from './MessageTable.js';
 
-// const messages = [
-//   {
-//     "id": 1,
-//     "subject": "You can't input the protocol without calculating the mobile RSS protocol!",
-//     "read": false,
-//     "starred": true,
-//     "labels": ["dev", "personal"]
-//   },
-//   {
-//     "id": 2,
-//     "subject": "connecting the system won't do anything, we need to input the mobile AI panel!",
-//     "read": false,
-//     "starred": false,
-//     "selected": true,
-//     "labels": []
-//   },
-//   {
-//     "id": 3,
-//     "subject": "Use the 1080p HTTP feed, then you can parse the cross-platform hard drive!",
-//     "read": false,
-//     "starred": true,
-//     "labels": ["dev"]
-//   },
-//   {
-//     "id": 4,
-//     "subject": "We need to program the primary TCP hard drive!",
-//     "read": true,
-//     "starred": false,
-//     "selected": true,
-//     "labels": []
-//   },
-//   {
-//     "id": 5,
-//     "subject": "If we override the interface, we can get to the HTTP feed through the virtual EXE interface!",
-//     "read": false,
-//     "starred": false,
-//     "labels": ["personal"]
-//   },
-//   {
-//     "id": 6,
-//     "subject": "We need to back up the wireless GB driver!",
-//     "read": true,
-//     "starred": true,
-//     "labels": []
-//   },
-//   {
-//     "id": 7,
-//     "subject": "We need to index the mobile PCI bus!",
-//     "read": true,
-//     "starred": false,
-//     "labels": ["dev", "personal"]
-//   },
-//   {
-//     "id": 8,
-//     "subject": "If we connect the sensor, we can get to the HDD port through the redundant IB firewall!",
-//     "read": true,
-//     "starred": true,
-//     "labels": []
-//   }
-// ]
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -81,11 +20,14 @@ class App extends Component {
     this.setState({ messages: result._embedded.messages });
   }
 
-  fetchMessages() {
-    fetch('http://localhost:8181/api/messages')
-    .then(response => response.json())
-    .then(result => this.setMessages(result))
-    .catch(e => e);
+  async fetchMessages() {
+    try {
+      const response = await fetch('http://localhost:8181/api/messages');
+      const result = await response.json();
+      this.setMessages(result);
+    } catch(err) {
+      console.error(err);
+    }
   }
 
   componentDidMount() {
